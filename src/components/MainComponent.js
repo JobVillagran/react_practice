704ea942a977c133be1a9a7ff97ae10f4ec579c4
JobV <jobvillagran@gmail.com>
2022-11-04 07:28:24 -0600

    Second Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,20 @@
-
 import React, { Component } from 'react';
+
 import Home from './HomeComponent';
-import About from './AboutComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
-import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import DishDetail from './DishdetailComponent';
+
+import { COMMENTS } from '../shared/comments'
+import { DISHES } from '../shared/dishes'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
+
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -19,57 +22,72 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            dishes: DISHES,
             comments: COMMENTS,
+            dishes: DISHES,
+            leaders: LEADERS,
             promotions: PROMOTIONS,
-            leaders: LEADERS
         };
+
     }
 
     render() {
+
         const HomePage = () => {
             return(
                 <Home 
-                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    dish={ this.state.dishes.filter( (dish)=>dish.featured )[0] }
+                    promotion={this.state.promotions.filter( (promotion)=>promotion.featured )[0] }
+                    leader={this.state.leaders.filter( (leader)=>leader.featured )[0] }
                 />
             );
-          }
+        };
 
-        const RenderLeader = () => {
-            return (
-                <RenderLeader>
-                    {/* <Media>
-
-                    </Media> */}
-                </RenderLeader>
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.state.leaders}
+                />
             );
-        }
-        
+        };
+
+
         const DishWithId = ({match}) => {
-            return (
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            return(
+                <DishDetail 
+                
+                dish={this.state.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+                comments={this.state.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
+                
                 
                 />
-            );   
-        }
+            );
+        };
+
+
+
         return (
             <div>
-                <Header />
+                <Header></Header>
+
                 <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path="/aboutus" component={About} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/home" component={ HomePage } />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> }/>
+
                     <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={Contact} />
+
+                    <Route exact path="/contactus" component={Contact } />
+                    <Route exact path="/aboutus" component={ AboutUsPage } />
+                   
+                    {/* if url dosesnt match, bydefault redirect to */}
                     <Redirect to="/home" />
                 </Switch>
-                <Footer />
-            </div>
+
+                <Footer></Footer>
+            </div> 
         );
+
     }
+
 }
 
 export default Main;
